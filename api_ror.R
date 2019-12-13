1fb60afbfc0556f16d7b7de35d3782118e06f488
Emilio M. Bruna <embruna@ufl.edu>
2019-12-13 14:54:07 -0500

    api ror

---
+++ api_ror.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(httr)
+library(jsonlite)
+path<-"https://api.ror.org/organizations"
+# r web / r json - get json data from url
+
+data <- fromJSON(path)
+
+"id"
+"name"
+"types"
+"links"
+"aliases"
+"acronyms"
+"status"
+"wikipedia_url"
+"labels"
+"country"
+"external_ids"
+"Wikidata"
+"GRID"
